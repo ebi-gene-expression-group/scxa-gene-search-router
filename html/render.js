5a1c9482aedff4659408c3e21d51382552020b83
lingyun1010 <zhaolingyun1010@gmail.com>
2018-09-05 10:38:15 +0100

    Wrap a router for root component

--- html/render.js
+++ html/render.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {BrowserRouter as Router} from 'react-router-dom';
 import MyComponent from '../src/index.js'
 
 const render = (options, target) => {
-  ReactDOM.render(<MyComponent {...options} />, document.getElementById(target))
+  ReactDOM.render(<Router><MyComponent {...options} /></Router>, document.getElementById(target))
 }
 
 export {render}
