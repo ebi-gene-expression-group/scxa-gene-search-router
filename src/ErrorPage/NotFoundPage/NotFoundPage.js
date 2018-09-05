fbf32442ae077d443ae8b8f4314a134578cc23c4
lingyun1010 <zhaolingyun1010@gmail.com>
2018-09-05 11:43:51 +0100

    Import CSS file

--- src/ErrorPage/NotFoundPage/NotFoundPage.js
+++ src/ErrorPage/NotFoundPage/NotFoundPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-//import './NotFoundPage.css';
+import './NotFoundPage.css';
 
 const Status = ({ code, children }) => (
   <Route
