25594eff176e001314617d9aef61d0bc41116478
lingyun1010 <zhaolingyun1010@gmail.com>
2018-09-05 11:46:05 +0100

    Add router history api fallback to webpack config

--- serve.config.js
+++ serve.config.js
@@ -1,15 +1,23 @@
 const path = require(`path`)
-module.exports = require(`./webpack.config.js`)
 
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
+
+
+module.exports = require(`./webpack.config.js`)
 module.exports.mode = `development`
 module.exports.devtool = `source-map`
 // Must match module.exports.serve.dev.publicPath or bad things may happen
-module.exports.output.publicPath = `/dist`
+module.exports.output.publicPath = `/dist/`
 
 module.exports.serve = {
   content: path.resolve(__dirname, `html`),
   devMiddleware: {
     publicPath: `/dist/`
+  },
+  // Make route request back to local port
+  add: (app, middleware, options) => {
+	app.use(convert(history()));
   },
 
   port: 9000
