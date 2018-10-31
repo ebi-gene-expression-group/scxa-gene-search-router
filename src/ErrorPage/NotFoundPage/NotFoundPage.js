43fd2f39353642468ba525635469e5f7dbcf28a7
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-10-31 11:15:37 +0000

    Replace CSS with styled-components

--- src/ErrorPage/NotFoundPage/NotFoundPage.js
+++ src/ErrorPage/NotFoundPage/NotFoundPage.js
@@ -1,29 +1,41 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React from 'react'
+import styled from 'styled-components'
+import { Route } from 'react-router'
 
-import './NotFoundPage.css';
+import './NotFoundPage.css'
 
-const Status = ({ code, children }) => (
+const Status = ({code, children}) =>
   <Route
     render={({ staticContext }) => {
       if (staticContext) staticContext.status = code;
       return children;
     }}
   />
-);
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const ActionDiv = styled.div`
+  margin-top: 20px;
+`
 
 const NotFoundPage = () => (
   <Status code={404}>
-    <div className="NotFoundPage">
+    <NotFoundContainer>
       <h1>Oops!</h1>
-      <div>Looks like you're lost...</div>
-      <div className="action">
-        <a className="btn btn-primary" href="/">
+      <div>Looks like you’re lost...</div>
+      <ActionDiv>
+        <a className={`btn btn-primary`} href={`/`}>
           Guide me to the right path!
         </a>
-      </div>
-    </div>
+      </ActionDiv>
+    </NotFoundContainer>
   </Status>
-);
+)
 
-export default NotFoundPage;
+export default NotFoundPage
